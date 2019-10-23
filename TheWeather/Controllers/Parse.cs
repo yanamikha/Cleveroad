e415391628a03ccf3fe3389e82512c5eb4ace329
yanamikha <yanamikha@gmail.com>
2019-10-23 15:10:23 +0300

    Add files via upload

---
+++ TheWeather/Controllers/Parse.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TheWeather.Controllers
+{
+    public partial class ValuesController
+    {
+        public static class Parse
+        {   
+            public static string CurrentDay(string str)
+            {
+                dynamic jsonDe = JsonConvert.DeserializeObject(str);
+                long timeUTC = jsonDe.dt;
+                int timeZone = jsonDe.timezone / 3600;           
+                string temperature = jsonDe.main.temp;                    
+                double windSpeed = jsonDe.wind.speed;
+                double clouds = jsonDe.clouds.all;
+                DateTime date = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(timeUTC).AddHours(timeZone);
+                return date.ToString("yyyy-MM-dd HH:mm:ss") + "\n\nTemperature: " + temperature + "°C\nWind speed:  " + windSpeed + " meter/sec\nCloudiness:  " + clouds + "%.";
+            }
+
+            public static string FiveDaysForecast(string str)            
+            {
+                dynamic jsonDe = JsonConvert.DeserializeObject(str);
+
+                DateTime[] timeISO = new DateTime[jsonDe.list.Count];
+                int[] min_temperature = new int[jsonDe.list.Count];
+                int[] max_temperature = new int[jsonDe.list.Count];
+                double[] windSpeed = new double[jsonDe.list.Count];
+                double[] clouds = new double[jsonDe.list.Count];
+
+                for (int i = 0; i < jsonDe.list.Count; i++)
+                {
+                    timeISO[i] = jsonDe.list[i].dt_txt;
+                    min_temperature[i] = jsonDe.list[i].main.temp_min;
+                    max_temperature[i] = jsonDe.list[i].main.temp_max;
+                    windSpeed[i] = jsonDe.list[i].wind.speed;
+                    clouds[i] = jsonDe.list[i].clouds.all;
+                }
+
+                string result = string.Empty;
+                for (int i = 0; i < jsonDe.list.Count; i++)
+                {
+                    if (timeISO[i].Hour == 12)
+                        result += timeISO[i] + "\nMin temperature: " + min_temperature[i] + "°C, Max temperature: " + min_temperature[i] + "°C\nWind speed:  " + windSpeed[i] + " meter/sec\nCloudiness:  " + clouds[i] + "%.\n\n";
+                }
+                return result;
+            }
+        
+        }
+    }
+}
---
+++ TheWeather/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheWeather.Controllers
+{
+    [Route("api")]
+    [ApiController]
+
+    public partial class ValuesController : ControllerBase
+    {
+        // GET api/GetCurrentWeather
+        [HttpGet("GetCurrentWeather")]
+        public string GetCurrentWeather(string city)
+        {
+            string line = string.Empty;
+            using (WebClient wc = new WebClient())
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(city, @"[a-z\-]$"))
+                {
+                    try
+                    {
+                        line = wc.DownloadString("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=58674fffe2de0c19d2c847c9cb4d01a9");
+                        return Parse.CurrentDay(line);
+                    }
+
+                    catch
+                    {
+                        return "Incorrect city";
+                    }
+                }
+                return "Incorrect city";
+            }         
+        }
+
+        // GET api/GetForecast
+        [HttpGet("GetForecast")]
+        public string GetForecast(string city)
+        {
+            string line = string.Empty;
+            using (WebClient wc = new WebClient())
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(city, @"[a-z\-]$"))
+                {
+                    try
+                    {
+                        line = wc.DownloadString("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=58674fffe2de0c19d2c847c9cb4d01a9");
+                        return Parse.FiveDaysForecast(line);
+                    }
+
+                    catch
+                    {
+                        return "Incorrect city";
+                    }
+                }
+                return "Incorrect city";
+            }
+        }
+    }
+}
+
